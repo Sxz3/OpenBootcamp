bd4d54270c2a71246553883ddb544df09b60a998
Sxz3 <137898653+Sxz3@users.noreply.github.com>
2023-06-27 14:49:19 +0200

    Add files via upload

---
+++ Ejercicio1/src/com/ejercicio/Mostrar.java
@@ -0,0 +1,29 @@
+package com.ejercicio;
+
+public class Mostrar {
+
+    public static void main(String[] args) {
+
+        int numero1=69;
+        long numero2=420;
+        short numero3=12;
+        double numero4=6.9;
+        byte numero5=7;
+        boolean dio=true;
+        float jot=25;
+        String stand="Star platinum";
+        char pan='a';
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(dio);
+        System.out.println(stand);
+        System.out.println(pan);
+        System.out.println(jot);
+
+
+    }
+}
